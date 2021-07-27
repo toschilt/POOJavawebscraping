25a02c0ab14c1f405fdba9b475220cae2baeb9e0
murilo-toddy <murilo.todao@usp.br>
2021-07-26 22:16:35 -0300

    Updates local_database nuances to work better with user interface

--- src/local_database/Main.java
+++ src/local_database/Main.java
@@ -4,25 +4,7 @@ public class Main {
     
     public static void main(String args[]) {
         
-        RegistersHandler reg = new RegistersHandler();
-
-        /*  
-			TODO Não permitir casos duplicados
-			Casos duplicados atualizam o database
-        */
-        
-        reg.registerNewCase(new String[] { "a", "b", "c", "d", "e", "j", "k" });
-        reg.registerNewCase(new String[] { "asd", "asd", "asd", "sad", "asd", "sad", "asd" });
-        
-        String namer = new String();
-        
-        try { namer = reg.searchForCase("a"); }
-        catch(Exception e) {}
-        
-        System.out.println(namer);
-     
-        
-        System.out.println("Done!");
+        System.out.println("l8r");
     }
 
 }
